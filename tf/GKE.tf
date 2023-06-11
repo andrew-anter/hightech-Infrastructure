f100ce909bbbf7be591790de8b463bd54dc36745
Andrew Anter Gebraeel <andrew.anter@gmail.com>
2023-06-11 12:43:01 +0300

    GKE uncommented

--- tf/GKE.tf
+++ tf/GKE.tf
@@ -1,65 +1,65 @@
 
 
-# # GKE cluster
-# resource "google_container_cluster" "primary" {
-#   name     = "${var.project_id}-gke"
-#   location = var.zone
+# GKE cluster
+resource "google_container_cluster" "primary" {
+  name     = "${var.project_id}-gke"
+  location = var.zone
   
-#   # We can't create a cluster with no node pool defined, but we want to only use
-#   # separately managed node pools. So we create the smallest possible default
-#   # node pool and immediately delete it.
-#   remove_default_node_pool = true
-#   initial_node_count       = 1
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
 
-#   private_cluster_config {
-#     enable_private_endpoint = true
-#     enable_private_nodes   = true 
-#     master_ipv4_cidr_block = "10.13.0.0/28"
-#   }
-#   ip_allocation_policy {
-#     cluster_ipv4_cidr_block  = "10.11.0.0/21"
-#     services_ipv4_cidr_block = "10.12.0.0/21"
-#   }
-#   master_authorized_networks_config {
-#     cidr_blocks {
-#       cidr_block   = "10.0.0.7/32"
-#       display_name = "network1"
-#     }
-#   }
-  
-
-#   network    = google_compute_network.vpc.name
-#   subnetwork = google_compute_subnetwork.subnet.name
-# }
-
-# # Separately Managed Node Pool
-# resource "google_container_node_pool" "primary_nodes" {
-#   name       = google_container_cluster.primary.name
-#   location   = var.zone
-#   cluster    = google_container_cluster.primary.name
-#   node_count = var.gke_num_nodes
-
+  private_cluster_config {
+    enable_private_endpoint = true
+    enable_private_nodes   = true 
+    master_ipv4_cidr_block = "10.13.0.0/28"
+  }
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block  = "10.11.0.0/21"
+    services_ipv4_cidr_block = "10.12.0.0/21"
+  }
+  master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block   = "10.0.0.7/32"
+      display_name = "network1"
+    }
+  }
   
 
-#   node_config {
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/logging.write",
-#       "https://www.googleapis.com/auth/monitoring",
-#     ]
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+}
+
+# Separately Managed Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = google_container_cluster.primary.name
+  location   = var.zone
+  cluster    = google_container_cluster.primary.name
+  node_count = var.gke_num_nodes
+
+  
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
     
-#     machine_type = var.machine_type
-#     disk_size_gb = var.disk_size
+    machine_type = var.machine_type
+    disk_size_gb = var.disk_size
 
-#     labels = {
-#       env = var.project_id
-#     }
-#     tags         = ["gke-node", "${var.project_id}-gke"]
+    labels = {
+      env = var.project_id
+    }
+    tags         = ["gke-node", "${var.project_id}-gke"]
     
-#     metadata = {
-#       disable-legacy-endpoints = "true"
-#     }
-#   }
-# }
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
 
 
 
