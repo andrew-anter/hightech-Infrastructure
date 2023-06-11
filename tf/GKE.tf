11f009eeee9fad3470e00ad07a5bc606a10a0526
Andrew Anter Gebraeel <andrew.anter@gmail.com>
2023-06-11 14:06:05 +0300

    GKE.tf updated

--- tf/GKE.tf
+++ tf/GKE.tf
@@ -22,8 +22,8 @@ resource "google_container_cluster" "primary" {
   }
   master_authorized_networks_config {
     cidr_blocks {
-      cidr_block   = "10.0.0.7/32"
-      display_name = "network1"
+      cidr_block   = "10.10.0.7/32"
+      display_name = "jumphost-network"
     }
   }
   
